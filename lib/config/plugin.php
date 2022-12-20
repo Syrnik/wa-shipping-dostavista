27893aedd1a4f854d0f3c7f4835b84035645e510
Serge Rodovnichenko <sergerod@gmail.com>
2022-12-20 15:11:45 +0300

    Задекларировал поддержку Premium

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,11 +1,13 @@
 <?php
 return array(
-    'name'             => 'Dostavista',
-    'description'      => 'Расчёт стоимости доставки курьерской службой Dostavista',
-    'icon'             => 'img/icon16.png',
-    'logo'             => 'img/dostavista.png',
-    'version'          => '1.0.1',
-    'vendor'           => '670917',
-    'external'         => true,
-    'services_by_type' => true
+    'name'                => 'Dostavista',
+    'description'         => 'Расчёт стоимости доставки курьерской службой Dostavista',
+    'icon'                => 'img/icon16.png',
+    'logo'                => 'img/dostavista.png',
+    'version'             => '1.0.1',
+    'vendor'              => '670917',
+    'external'            => true,
+    'services_by_type'    => true,
+    'fractional_quantity' => true,
+    'stock_units'         => true
 );

a0b28d0c4fed5bbd2039672be03b9a74f5e441a5
Serge Rodovnichenko <sergerod@gmail.com>
2023-10-31 01:15:22 +0300

    Настройки для UI2: начало

---
+++ build/ui2/settings/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import {reactive} from "vue";
+
+const props = defineProps(['info', 'settings']);
+const setting = reactive(props.settings);
+
+</script>
+
+<template>
+  <div class="fields">
+
+  </div>
+</template>

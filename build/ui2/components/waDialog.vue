991580e83729f20b139c5008c8b9dc607ea09dee
Serge Rodovnichenko <serge@syrnik.com>
2023-11-09 00:30:28 +0300

    region select modal works fine

--- build/ui2/components/waDialog.vue
+++ build/ui2/components/waDialog.vue
@@ -26,12 +26,14 @@ function onResize() {
 function setPosition() {
     let dialog_width = dialog_body.value.offsetWidth,
         dialog_height = dialog_body.value.offsetHeight;
-    dialog_body.value.style.height = dialog_content.value.style.height = null;
+    dialog_body.value.style.height = null;
     const pad = 20,
         position = {
             left: Math.floor((window.innerWidth - dialog_width) / 2),
             top: Math.floor((window.innerHeight - dialog_height) / 2)
         };
+
+    dialog_body.value.style['max-height']=`calc(100% - ${pad*2}px)`;
 
     if (position.left > 0) {
         if (position.left + dialog_width > window.innerWidth) {
@@ -43,13 +45,9 @@ function setPosition() {
         if (position.top + dialog_height > window.innerHeight) {
             position.top = window.innerHeight - dialog_height - pad;
         }
-        content_class.value["is-long-content"] = false;
-        dialog_content.value.style.height = null;
     } else {
         position.top = pad;
-        content_class.value["is-long-content"] = true;
         dialog_body.value.style.height = `${window.innerHeight - pad * 2}px`;
-        dialog_content.value.style.height = `${dialog_body.value.offsetHeight - (dialog_header.value?.offsetHeight ?? 0) - (dialog_footer.value?.offsetHeight ?? 0)}px`
     }
     dialog_body.value.style.left = `${position.left}px`;
     dialog_body.value.style.top = `${position.top}px`;
@@ -74,7 +72,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div class="dialog" style="display: block">
+    <div class="dialog w-syrnik-dialog" style="display: block">
         <div ref="dialog_background" class="dialog-background"></div>
         <div ref="dialog_body" class="dialog-body">
             <a class="dialog-close" href="#" @click.prevent="$emit('close')"><i class="fas fa-times"></i></a>
@@ -91,6 +89,14 @@ onUnmounted(() => {
     </div>
 </template>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
+.w-syrnik-dialog
+
+  > .dialog-body
+    display flex
+    flex-direction column
+
+    > .dialog-content
+      flex 1
 
 </style>
--- build/ui2/settings/OperatingRegionsModal.vue
+++ build/ui2/settings/OperatingRegionsModal.vue
@@ -9,17 +9,55 @@ const props = defineProps({value: {type: Array, default: () => []}, regionList: 
 </script>
 
 <template>
-    <wa-dialog @close="$emit('close')">
-        <ul class="w-shipping-dostavista-region-list">
-            <li class="w-shipping-dostavista-region-list-item" v-for="r in regionList">
-                <label><input type="checkbox" v-model="selection" :value="r.code">
-                    {{ r.name }}
-                </label>
-            </li>
-        </ul>
-    </wa-dialog>
+  <wa-dialog @close="$emit('close')" class="w-shipping-dostavista-operating-region-dialog">
+    <template #header><h3>Выберите регион</h3></template>
+      <template #footer>
+          <button class="button">Выбрать</button>
+          <button class="button gray">отмена</button>
+      </template>
+    <ul class="w-shipping-dostavista-region-list">
+      <li class="w-shipping-dostavista-region-list-item" v-for="r in regionList">
+        <label>
+            <span class="wa-checkbox">
+                <input type="checkbox" v-model="selection" :value="r.code">
+                <span>
+                    <span class="icon">
+                        <i class="fas fa-check"></i>
+                    </span>
+                </span>
+            </span>
+          {{ r.name }}
+        </label>
+      </li>
+    </ul>
+  </wa-dialog>
 </template>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
+.w-shipping-dostavista-operating-region-dialog
 
+  &.dialog
+    .dialog-body
+      left 0
+      right 0
+      top 0
+      bottom 0
+      width auto
+
+  .w-shipping-dostavista-region-list
+    list-style-type none
+
+    @media screen and (min-width: 1280px)
+      column-count: 3;
+      column-gap: 1em;
+
+    @media screen and (min-width: 960px) and (max-width: 1279px)
+      column-count: 2;
+      column-gap: 0.75em;
+
+  .w-shipping-dostavista-region-list-item
+    padding 0.25em 3px
+
+    label
+      display block
 </style>

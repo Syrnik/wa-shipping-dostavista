baf61b07c591896bb191ac723d279d31d241cfb6
Serge Rodovnichenko <serge@syrnik.com>
2023-11-10 10:22:44 +0300

    desired time and date

--- build/ui2/settings/App.vue
+++ build/ui2/settings/App.vue
@@ -1,21 +1,31 @@
 <script setup>
 
-import {reactive} from "vue";
+import {onMounted, reactive} from "vue";
 import TokenApi from "./TokenApi.vue";
 import OperatingRegion from "./OperatingRegion.vue";
 import LocationFrom from "./LocationFrom.vue";
+import ServerType from "./ServerType.vue";
+import DeliveryTimeControl from "./DeliveryTimeControl.vue";
+import CustomerInterval from "./CustomerInterval.vue";
 
 const props = defineProps(['info', 'settings']);
 const setting = reactive(props.settings);
+
+onMounted(()=> document.getElementsByClassName('article')?.[0]?.classList.add('wider'));
 
 </script>
 
 <template>
   <div class="fields">
     <div class="fields-group">
+      <server-type v-model="setting.api_server"/>
       <token-api v-model.trim="setting.token"/>
       <operating-region v-model="setting.operating_regions" :region-list="info.lists.regions"/>
       <location-from v-model="setting.location_from.name"/>
+    </div>
+    <div class="fields-group">
+      <delivery-time-control v-model="setting.delivery_time" v-model:exact-hours="setting.exact_delivery_time"/>
+        <customer-interval v-model="setting.customer_interval" />
     </div>
   </div>
 </template>
---
+++ build/ui2/settings/CustomerIntervalItem.vue
@@ -0,0 +1,53 @@
+<script setup>
+import {ref} from "vue";
+import addns from "../components/addns";
+
+const props = defineProps({
+    modelValue: {
+      type: Object, default: () => ({
+        from: '10',
+        from_m: '00',
+        to: '12',
+        to_m: '00',
+        day: [1, 2, 3, 4, 5],
+        workday: false,
+        holiday: false
+      })
+    },
+    ns: {type: String, default: ''}
+  }),
+  emit = defineEmits(['update:modelValue', 'delete']),
+  interval = ref(props.modelValue);
+
+function emitChanges() {
+  emit('update:modelValue', JSON.parse(JSON.stringify(interval.value)));
+}
+</script>
+
+<template>
+  <tr>
+    <td class="nowrap">с
+      <input type="text" class="shortest w-shipping-dostavista__hour-minute-input align-right" :name="addns('from', ns)"
+             v-model.trim="interval.from" @input="emitChanges"> :
+      <input type="text" class="shortest align-right w-shipping-dostavista__hour-minute-input" :name="addns('from_m', ns)"
+             v-model.trim="interval.from_m" @input="emitChanges">
+    </td>
+    <td class="nowrap">до
+      <input type="text" class="shortest w-shipping-dostavista__hour-minute-input align-right" :name="addns('to', ns)"
+             v-model.trim="interval.to" @input="emitChanges"> :
+      <input type="text" class="shortest align-right w-shipping-dostavista__hour-minute-input" :name="addns('to_m', ns)"
+             v-model.trim="interval.to_m" @input="emitChanges">
+    </td>
+    <td v-for="v in [1,2,3,4,5,6,7]"><input type="checkbox" :name="addns('', addns('day', ns))" :value="v"
+                                            v-model="interval.day" @change="emitChanges"></td>
+    <td><input type="checkbox" :name="addns('holiday', ns)" v-model="interval.holiday" @change="emitChanges"></td>
+    <td><input type="checkbox" :name="addns('workday', ns)" v-model="interval.workday" @change="emitChanges"></td>
+      <td><a class="inline-link"
+             @click.prevent="$emit('delete')"><i class="icon16 delete fa fa-trash-alt text-red"></i></a></td>
+  </tr>
+</template>
+
+<style lang="stylus">
+input.w-shipping-dostavista__hour-minute-input
+  width 2.5rem
+</style>

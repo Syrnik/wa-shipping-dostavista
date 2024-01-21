94bf4a49776962644b7d0fa2cb645d9f27b1e7c2
Serge Rodovnichenko <serge@syrnik.com>
2024-01-21 23:39:00 +0300

    Delete unused vendor

---
+++ lib/updates/2.0/1705869461.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @copyright Serge Rodovnichenko, 2023
+ * @license Webasyst
+ */
+
+$dir = wa()->getConfig()->getPath('plugins') . '/shipping/dostavista/';
+
+$files = [
+    'lib/vendors/syrnik/wa-sipping-utils/lib'
+];
+
+foreach ($files as $file) {
+    try {
+        waFiles::delete($dir . $file);
+    } catch (Exception $e) {
+        waLog::log('Dostavista shipping plugin update (' . __FILE__ . "): Error deleting old file $file: " . $e->getMessage());
+    }
+}
